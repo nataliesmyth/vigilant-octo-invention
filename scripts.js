e9fbad154c5bd90b6127393fc4c44758a973a01b
Nataliesmyth <nataliesmyth11@gmail.com>
2022-12-06 17:32:48 -0500

    created flip effect

--- scripts.js
+++ scripts.js
@@ -4,6 +4,11 @@ const cards = document.querySelectorAll('.memory-card');
 
 function flipCard() {
     console.log('i was clicked')
+    // in this context, the this keyword represents the element that fired the click event, <div class="memory-card"></div
+    console.log(this)
+    // we want to access the class list of the memory cards and toggle the flip class
+    // toggle means if it's there, remove it, if it's not, add it.
+    this.classList.toggle('flip')
 }
 // loop through the list of cards and attach an event listener to each one
 // we want to listen to a click event, and every time that event happens we will execute a function called flipCard, declared above.
