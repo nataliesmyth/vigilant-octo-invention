b0dc286528c5291b6d6e383e75ac4f58f9955840
Nataliesmyth <nataliesmyth11@gmail.com>
2022-12-06 17:48:37 -0500

    flip functionality

--- scripts.js
+++ scripts.js
@@ -2,13 +2,28 @@
 // first, make a list of all memory card elements and store it in a constant variable:
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
 function flipCard() {
     console.log('i was clicked')
     // in this context, the this keyword represents the element that fired the click event, <div class="memory-card"></div
-    console.log(this)
+    // console.log(this)
     // we want to access the class list of the memory cards and toggle the flip class
-    // toggle means if it's there, remove it, if it's not, add it.
-    this.classList.toggle('flip')
+    this.classList.add('flip')
+
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+
+        console.log(hasFlippedCard, firstCard)
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // check if cards match
+    }
 }
 // loop through the list of cards and attach an event listener to each one
 // we want to listen to a click event, and every time that event happens we will execute a function called flipCard, declared above.
